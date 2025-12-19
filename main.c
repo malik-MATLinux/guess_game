e4b31b01508959513bb4f01bafed9c7de3bff4ac
Malik Adebayo <malik.adebayo@matlinux.com>
2025-12-19 16:20:42 +0000

    A very basic number game, few bugs needs checking as i work on it

--- main.c
+++ main.c
@@ -2,9 +2,70 @@
 #include <stdlib.h>
 #include <time.h>
 
+void guess(int input, int answer, int attempts, int chances);
+
 int main(){
 
-    printf("hello world");
+    srand(time(NULL));
 
+    int input;
+    int attempts = 1;
+    int chances = 15;
+    int tries = 5;
+    int min = 1;
+    int max = 100;
+
+    int answer = (rand() % (max - min + 1)) + min;
+
+    printf("######### Welcome to the game of guessing numbers ########\n\n");
+    
+    printf("        £nter your gu£$$    :");
+    scanf("%d", &input);
+
+    guess(input, answer, attempts, chances);
     return 0;
+}
+
+void guess(int input, int answer, int attempts, int chances){
+    for (int i = 0; i < chances; i++)
+        {
+            if (input == answer)
+            {
+                printf("\n\n       #############################          \n           TADAAAAAA , Correct   \n      #############################\n\n\n");
+                break;
+            }
+            else if (input < answer)
+            {
+                printf("Your guess is too low. Try again:   ");
+                attempts ++;
+                scanf("%d", &input);
+            }
+            else if (input > answer)
+            {
+                printf("Your guess is too high. Try again:   ");
+                attempts ++;
+                scanf("%d", &input);
+            }
+            else
+            {
+                if (attempts >= chances && attempts <= chances)
+                {
+                    printf("Sorry, you've used all your attempts. The correct answer was %d.\n\n\n\n", answer);
+                }else{
+
+                    printf("OH OH..... Wrong guess, Try again:   ");
+                    attempts ++;
+                    scanf("%d", &input); 
+                }
+                
+                
+            }
+            
+            
+        }
+}
+
+void cald_attempts(int total_attempts){
+
+
 }
